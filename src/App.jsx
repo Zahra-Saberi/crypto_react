f8acc7e6000c4ce2357da7229148586e0afb4b23
Zahra <zahraas1025@gmail.com>
2023-01-27 16:46:16 +0330

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import FooterComponent from "./components/ui/Footer.component";
+import NavbarComponent from "./components/ui/Navbar.component";
 
 
 function App() {
   return (
-    <div className="App">
-      <h3>Cryptocurrency</h3>
+    <div>
+      {/* Navbar */}
+      <NavbarComponent></NavbarComponent>
+
+      {/* Wrapper */}
+      <div className="wrapper">
+        <h2>Wrapper</h2>
+      </div>
+      
+
+      {/* Footer */}
+      <FooterComponent></FooterComponent>
     </div>
   );
 }
