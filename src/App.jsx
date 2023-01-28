5eb888417372553209c2b9318a1fdcdd3720f3fc
Zahra <zahraas1025@gmail.com>
2023-01-28 16:41:14 +0330

    routes implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import FooterComponent from "./components/ui/Footer.component";
 import NavbarComponent from "./components/ui/Navbar.component";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home.page";
+import FAQPage from "./pages/FAQ.page";
+import ContactUsPage from "./pages/ContactUs.page";
+import AboutUsPage from "./pages/AboutUs.page";
 
 
 function App() {
@@ -10,7 +15,12 @@ function App() {
 
       {/* Wrapper */}
       <div className="wrapper">
-        <h2>Wrapper</h2>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/contact-us" element={<ContactUsPage />} />
+        </Routes>
       </div>
       
 
